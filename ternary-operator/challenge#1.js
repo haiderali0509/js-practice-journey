f2975858ad75923ab65c67f670532f5ea92f8895
Haider Ali <chhaiderali0509@gmail.com>
2025-10-10 16:13:32 +0500

    done challenge #2 add functionality by letting the player know if their guess was too high,too low or exactly right

--- ternary-operator/challenge#1.js
+++ ternary-operator/challenge#1.js
@@ -1,9 +1,26 @@
 // Challenge #1
 // Refactor if else statement to use Ternary operator
-const playerGuess = 3;
+
+// Challenge #2
+// Now improve the functionality of this code by
+// letting the player know if their guess was too high,
+// too low, or exactly right.
+const playerGuess = 15;
 const correctAnswer = 6;
 
-let message = playerGuess === correctedAnswer ? "Correct" : "Wrong";
+let message =
+  playerGuess === correctAnswer
+    ? "Exactly Right"
+    : playerGuess < correctAnswer
+    ? "Too Low"
+    : "To High";
+// let message =
+//   playerGuess > correctAnswer
+//     ? "Too High"
+//     : playerGuess < correctAnswer
+//     ? "Too Low"
+//     : "Exactly Right";
+
 // if (playerGuess == correctAnswer) {
 //   message = "Correct";
 // } else {
